799e75dd4cb1b90d060303b1d9e22fc8ebbbe024
David Alienyi <alienyidavid4christ@gmail.com>
2020-07-31 03:14:58 +0100

    fix

--- Helper.js
+++ Helper.js
@@ -112,37 +112,39 @@ const puppeteer = require('puppeteer');
                     '--disable-setuid-sandbox',
                   ],
             });
-            const page = await browser.newPage();
-           
-            await page.setDefaultNavigationTimeout(0);
-           
-              await page.goto('https://www.soccer24.com/', {waitUntil: 'networkidle2'});
-          
-              const text = await page.$$eval('.event__match', (options) => {
-          
-                  const result = options.map(option => option.innerText.split("\n"));
-                  return result;
-              });
-          
-              var data = [];
-              let match;
-              text.forEach(function(item, index) {
-                  match = {}
-                  if (item[0] == 'Finished' || !isNaN(item[0].trim()) || item[0] == 'Half Time') {
-                      match.time = item[0];
-                      match.home_name = item[1];
-                      match.away_name = item[2];
-                      match.ft_score  = item[3] + item[4] + item[5];
-                      data.push(match);
-                  } else if (item[0] == 'Cancelled' || item[0] == 'Postponed') {
-                      match.time = item[0];
-                      match.home_name = item[1];
-                      match.away_name = item[2];
-                      data.push(match);
-                  }
-              });
-          
-              await browser.close();
+            
+          const page = await browser.newPage();
+         
+          await page.setDefaultNavigationTimeout(0);
+         
+            await page.goto('https://www.soccer24.com/', {waitUntil: 'networkidle2'});
+        
+            const text = await page.$$eval('.event__match', (options) => {
+        
+                const result = options.map(option => option.innerText.split("\n"));
+                return result;
+            });
+        
+            var data = [];
+            let match;
+            text.forEach(function(item, index) {
+                match = {}
+                if (item[0] == 'Finished' || !isNaN(item[0].trim()) || item[0] == 'Half Time') {
+                    match.time = item[0];
+                    match.home_name = item[1];
+                    match.away_name = item[2];
+                    match.score  = item[3] + item[4] + item[5];
+                    data.push(match);
+                } else if (item[0] == 'Cancelled' || item[0] == 'Postponed') {
+                    match.time = item[0];
+                    match.home_name = item[1];
+                    match.away_name = item[2];
+                    data.push(match);
+                }
+            });
+        
+            await browser.close();
+        
           
               return res.send({data: data, success: true});
           })();          
